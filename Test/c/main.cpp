31651cc83a9c2090b6b03984db76e1ce07b736af
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-07-26 21:03:09 +0800

    feat:实现objs移动，显示动态效果

--- Test/c/main.cpp
+++ Test/c/main.cpp
@@ -8,43 +8,61 @@
 using namespace std;
 using namespace std::chrono;
 
+struct point
+{
+    int x;
+    int y;
+};
+struct obj
+{
+    std::string name;
+    std::string url;
+    std::vector<point> points;
+};
+
+std::string format_json(std::vector<obj> objs)
+{
+    auto json_head = R"({"version": "1.0.0","type" : "update","content" : [)";
+    auto json_tail = R"(]})";
+    std::string json_body;
+    for (auto& obj : objs)
+    {
+        json_body += R"({"name": ")" + obj.name + R"(","url" : ")" + obj.url + R"(","points" : [)";
+        for (auto& point : obj.points)
+        {
+            json_body += R"({"x": )" + std::to_string(point.x) + R"(,"y" : )" + std::to_string(point.y) + R"(})";
+            if (&point != &obj.points.back())
+            {
+                json_body += ",";
+            }
+        }
+        json_body += R"(]})";
+        if (&obj != &objs.back())
+        {
+            json_body += ",";
+        }
+    }
+    return json_head + json_body + json_tail;
+}
+
 int main()
 {
 	TianLiTruthEye_CreateWindow();
 	TianLiTruthEye_ShowWindow();
 
-	std::string json_str = R"({
-        "version": "1.0.0",
-        "type" : "update",
-        "content" : [
-    {
-        "name": "风晶蝶",
-            "url" : "http://assets.yuanshen.site/icons/59.png",
-            "points" : [
-        {
-            "x": 10,
-                "y" : 10
-        },
-                    {
-                        "x": 12,
-                        "y" : 12
-                    }
-            ]
-    },
-            {
-                "name": "宝箱",
-                "url" : "http://assets.yuanshen.site/icons/59.png",
-                "points" : [
-                   {
-                        "x": 20,
-                        "y" : 30
-                    }
-                ]
-            }
-        ]
-})";
+    std::vector<obj> objs;
+    objs.push_back({ "风晶蝶", "http://assets.yuanshen.site/icons/59.png", { {10, 10}, {12, 12} } });
+    objs.push_back({ "宝箱", "http://assets.yuanshen.site/icons/59.png", { {20, 30} } });
+    //
 
-    SetJsonParams(json_str.c_str(), json_str.size());
+for (int i = 0; i < 10; i++)
+    {
+        objs[0].points[0].x += 5;
+    auto json_str = format_json(objs);
+        SetJsonParams(json_str.c_str(), json_str.size());
+    this_thread::sleep_for(100ms);
+
+    }
         
 	this_thread::sleep_for(2s);
 	//TianLiTruthEye_HideWindow();
