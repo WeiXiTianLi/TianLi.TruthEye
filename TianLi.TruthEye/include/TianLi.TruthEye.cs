127526be2abd991c75318ae6e118c257542b94ab
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-07-26 21:30:31 +0800

    update:同步接口定义

--- TianLi.TruthEye/include/TianLi.TruthEye.cs
+++ TianLi.TruthEye/include/TianLi.TruthEye.cs
@@ -7,16 +7,19 @@ namespace TianLi
 {
     public class TruthEye
     {
-        [DllImport("TianLi.TruthEye.dll")]
-        public static extern void TianLiTruthEye_CreateWindow();
+        [DllImport("TianLi.TruthEye.dll", EntryPoint = "TianLiTruthEye_CreateWindow", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void CreateWindow();
 
-        [DllImport("TianLi.TruthEye.dll")]
-        public static extern void TianLiTruthEye_DestroyWindow();
+        [DllImport("TianLi.TruthEye.dll", EntryPoint = "TianLiTruthEye_DestroyWindow", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DestroyWindow();
 
-        [DllImport("TianLi.TruthEye.dll")]
-        public static extern void TianLiTruthEye_ShowWindow();
+        [DllImport("TianLi.TruthEye.dll", EntryPoint = "TianLiTruthEye_ShowWindow", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ShowWindow();
 
-        [DllImport("TianLi.TruthEye.dll")]
-        public static extern void TianLiTruthEye_HideWindow();
+        [DllImport("TianLi.TruthEye.dll", EntryPoint = "TianLiTruthEye_HideWindow", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void HideWindow();
+
+        [DllImport("TianLi.TruthEye.dll", EntryPoint = "TianLiTruthEye_SetJsonParams", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetJsonParams(byte[] json_buff, int buff_size);
     }
 }
