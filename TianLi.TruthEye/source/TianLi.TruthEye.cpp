2f0428cf45698073c48e04bd69714bf64e55e458
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-10-09 21:43:23 +0800

    feat:添加写入日志

--- TianLi.TruthEye/source/TianLi.TruthEye.cpp
+++ TianLi.TruthEye/source/TianLi.TruthEye.cpp
@@ -1,4 +1,4 @@
-// QApp.cpp: 定义动态链接库的实现
+﻿// QApp.cpp: 定义动态链接库的实现
 //
 
 #include "../include/TianLi.TruthEye.h"
@@ -23,7 +23,25 @@
 #include <algorithm>
 // win
 #include <Windows.h>
-
+// spdlog
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/dup_filter_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+void init_logger()
+{
+    static bool is_inited = false;
+    if (is_inited)
+    {
+        return;
+    }
+    std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("TianLi.TruthEye.log", 1024 * 1024 * 5, 3));
+    sinks.push_back(std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5)));
+    auto logger = std::make_shared<spdlog::logger>("TianLi.TruthEye.Impl", sinks.begin(), sinks.end());
+    spdlog::set_default_logger(logger);
+    spdlog::flush_every(std::chrono::seconds(1));
+    spdlog::flush_on(spdlog::level::warn);
+}
 static QApplication *g_app = nullptr;
 static tePopupWindow *g_window = nullptr;
 static std::thread g_thread;
@@ -37,19 +55,24 @@ static void CreateQApplication()
     char *argv[] = {(char *)modulePath};
     g_app->setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
+    spdlog::info("创建窗口事件循环 {}", modulePath);
     g_app = &app;
     g_window = new tePopupWindow();
     // 窗口创建完成
     g_cv.notify_one();
     g_app->exec();
+    spdlog::info("窗口事件循环结束");
+    spdlog::shutdown();
 }
 
 void TianLiTruthEye_CreateWindow()
 {
+    init_logger();
     if (g_app != nullptr)
     {
         return;
     }
+    spdlog::info("悬浮窗创建");
     g_thread = std::thread(CreateQApplication);
     // 等待窗口创建完成
     std::unique_lock lock(g_mutex);
@@ -62,10 +85,11 @@ void TianLiTruthEye_DestroyWindow()
     {
         return;
     }
+    spdlog::info("悬浮窗销毁");
     g_app->exit(0);
     if (g_thread.joinable())
     {
-    g_thread.join();
+        g_thread.join();
     }
 }
 
